9125a2cd91668ca8d8e2801c04f1c9ff03bdfef1
Daniel Rodriguez <daniel@danielrodriguez.name>
2015-11-17 09:34:01 -0500

    Cleanup: Comments in RxViewController.

--- Test/Rx/RxViewController.swift
+++ Test/Rx/RxViewController.swift
@@ -12,14 +12,29 @@ import RxSwift
 
 public class RxViewController : UIViewController {
     
+    /// The tableview.
     var tableView:UITableView!
+    
+    /// The frame of the Playground you want to initialize.
     var playgroundFrame:CGRect?
+    
+    /// Rx bag of wonders...
     let disposeBag = DisposeBag()
     
+    /// Reactive datasource, used in the table view delegate.
     var dataSource: RxTableViewReactiveSectionModelArrayDataSourceSequenceWrapper<[RowDesc]>!
     
+    /// Our ViewModel, initialized with its default values
     var viewModel:RxViewModel = RxViewModel()
     
+    /**
+     Initializer for Playground testing.
+     
+     - parameter viewModel:       A ViewModel you initialize in the playground
+     - parameter playgroundFrame: The frame of the playground's active view.
+     
+     - returns: An instance of this
+     */
     public init(viewModel:RxViewModel, playgroundFrame:CGRect?) {
         super.init(nibName: nil, bundle: nil)
         self.playgroundFrame = playgroundFrame
@@ -52,8 +67,19 @@ extension RxViewController {
         setupRx()
     }
     
+    /**
+     Setup our reactive datasource, perform the reactive binding between the ViewModel and the dataSource, 
+     plus binding between our tableview and its delegate (this class).
+     */
     func setupRx() {
         
+        /**
+        *  A variant of Rx's reactive UITableView datasource that can handle our ViewModel's models (type RowDesc).
+        * 
+        *  It has two closures:  A cellFactory and a sectionHeaderViewFactory.
+        *
+        *  @return A Reactive datasource.
+        */
         self.dataSource = RxTableViewReactiveSectionModelArrayDataSourceSequenceWrapper(cellFactory: { (tv, s, r, item) -> UITableViewCell in
             // setup a cell via Rx
             let indexPath = NSIndexPath(forItem: r, inSection: s)
@@ -65,14 +91,19 @@ extension RxViewController {
             return item.state == .Selected ? 1 : 0
         }
         
+        // In here we connect the rows Rx property to the datasource.
         self.viewModel.rows.asObservable()
             .bindTo(tableView.rx_itemsWithDataSource(dataSource))
             .addDisposableTo(disposeBag)
         
+        // Installs the delegate (this class) as a forwarding delegate on rx_delegate.
+        // (normal delegate mechanism with reactive delegate mechanism)
         tableView.rx_setDelegate(self)
             .addDisposableTo(disposeBag)
     }
 }
+
+// MARK: - UITableViewDelegate methods
 
 extension RxViewController : UITableViewDelegate {
     
@@ -81,26 +112,14 @@ extension RxViewController : UITableViewDelegate {
     }
     
     public func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        
         guard let sectionDesc = self.dataSource.sectionAtIndex(section) else {
             return nil
         }
-        
         return headerInSection(sectionDesc)
     }
     
     public func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        
         return UIConstants.rowHeight
     }
     
 }
-
-// }
-
-
-
-
-
-
-
