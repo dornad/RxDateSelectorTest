339c48807b504bbf4ba989e740e6fd13d9ad1d8d
Daniel Rodriguez <daniel@danielrodriguez.name>
2015-11-17 09:24:56 -0500

    Cleanup: Removes unused files

--- RxSwift.playground/Contents.swift
+++ RxSwift.playground/Contents.swift
@@ -3,33 +3,3 @@
 import UIKit
 
 
-func distinctUntilChanged(lhs:NSDate?, rhs:NSDate?) -> Bool {
-    
-    guard let rhs = rhs else {
-        return false
-    }
-    
-    if let lhs = lhs {
-        
-        return abs( lhs.timeIntervalSinceDate(rhs) ) <= 60
-    }
-    
-    return true
-}
-
-// these should be false
-
-distinctUntilChanged(nil, rhs: nil)
-
-// these should be true
-
-distinctUntilChanged(nil, rhs: NSDate())
-distinctUntilChanged(NSDate(), rhs: NSDate())
-distinctUntilChanged(NSDate(), rhs: NSDate(timeIntervalSinceNow: 10))
-
-// these should be false, again
-
-distinctUntilChanged(NSDate(), rhs: NSDate(timeIntervalSinceNow: 100))
-distinctUntilChanged(NSDate(), rhs: NSDate(timeIntervalSinceNow: 1000))
-distinctUntilChanged(NSDate(), rhs: NSDate(timeIntervalSinceNow: 10000))
-distinctUntilChanged(NSDate(), rhs: NSDate(timeIntervalSinceNow: 100000))
